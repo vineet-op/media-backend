beacc4f226b4123491e4da806da1dada880d3775
Vineet Jadhav <vineet290403@gmail.com>
2026-05-31 13:16:23 +0530

    Update user controller to secure cookies for login and logout, and add new subscriber model for managing subscriptions.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -115,7 +115,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: false
+        secure: true
     }
 
     return res.status(200)
@@ -147,7 +147,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: false
+        secure: true
     }
 
     return res
---
+++ src/models/subscriber.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscribtionSchema = new Schema({
+
+    subscriber: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+    channel: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+}, {
+    timestamps: true
+})
+
+
+export const Subscriber = mongoose.model("Subscriber", subscribtionSchema)
