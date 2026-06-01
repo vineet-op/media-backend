e4a639d03b5d7a3ac7eea69222cf85bc83c6ce21
Vineet Jadhav <vineet290403@gmail.com>
2026-06-01 23:42:00 +0530

    Enhance user routes by adding new endpoints for password change, current user retrieval, account updates, avatar and cover image uploads, and watch history retrieval. Import necessary controller functions to support these features.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    getUserChannelInfo,
+    getWatchHistory,
+    updateAvatarImage,
+    updateCoverImage
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/jwt.middleware.js";
 
@@ -25,5 +37,19 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatarImage)
+
+router.route("/update-cover").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelInfo)
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 export default router
